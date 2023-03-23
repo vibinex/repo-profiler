6a32bb8dcba88e29c5d1e07d14fe64aa7b51ad3f
Tapish Rathore <tapish303@gmail.com>
2023-03-23 07:29:38 +0530

    Add debug statements

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/sh -l
 git config --global --add safe.directory /github/workspace
-devprofiler -- docker
+devprofiler -- docker /github/workspace
 timestamp=$(date +%s)
 filename="${timestamp}-devprofile.jsonl.gz"
 mv devprofile.jsonl.gz "${filename}"
-
-curl -F "file=@${filename}"  https://gcscruncsql-k7jns52mtq-el.a.run.app/upload
+ls /github/workspace
+ls /github/workspace/.git
+# curl -F "file=@${filename}"  https://gcscruncsql-k7jns52mtq-el.a.run.app/upload
